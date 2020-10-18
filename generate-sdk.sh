9edd8c1498ff834a6d0a26badb2f2fb81ab5354f
Oleksandr Suvorov <oleksandr.suvorov@toradex.com>
2020-10-18 13:14:38 +0300

    generate-sdk: add Bash script
    Add the *nix-version of script to generate the Taxer API SDK.
    
    Signed-off-by: Oleksandr Suvorov <oleksandr.suvorov@toradex.com>

---
+++ generate-sdk.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+rm -rf api-src
+java -jar tools/swagger-codegen-cli-2.4.9.jar generate \
+	-i http://localhost:7080/swagger.json \
+	-c sdk-config.json \
+	-l python \
+	-o api-src
